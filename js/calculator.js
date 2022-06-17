ee22e6b62152ab855729c856a071b30483ffd60a
marcosmena.dev <82055226+marcosmenadev@users.noreply.github.com>
2022-06-17 02:02:51 +0200

    calculate function
    calcula el gs. chupala jony

--- js/calculator.js
+++ js/calculator.js
@@ -3,34 +3,64 @@ document.getElementById("type").addEventListener('change' , type);
 // document.getElementById("reset").addEventListener('click', reset);
 
 
-let stats = [];
+let stats_names = ["atk_flat","atk","hp_flat","hp","def_flat","def","spd","cc","cdmg","res","eff"];
 
-let atk_flat = document.querySelector("atk_flat");
-let hp_flat = document.querySelector("hp_flat");
-let def_flat = document.querySelector("def_flat");
+/*  jony's shit
 
-let atk = document.querySelector("atk");
-let hp = document.querySelector("hp");
-let def = document.querySelector("def");
-let cc = document.querySelector("cc");
-let cdmg = document.querySelector("cdmg");
-let spd = document.querySelector("spd");
-let eff = document.querySelector("eff");
-let res = document.querySelector("res");
+let value = document.getElementById("type").value;
+
+
 
 function type() {
-    let value = document.getElementById("type").value;
+    
     if(value > 0){
         console.log("funciona el type", value );
     }
     else{
         return false;
     }
+}*/
+
+function generate_stats(){
+    let stats = [];
+    for(let i=0;i<4;i++){
+        stats[i]= [stats_names[document.getElementById("stat"+(i+1)).selectedIndex-1], document.getElementById("value_stat"+(i+1)).value];
+    }
+    return stats
 }
+/*
+function load_card(gs){
 
 
+}*/
 
+function calculate() {
 
-// calculate() {
+    stats = generate_stats();
+    let gs = 0;
+    for (let i=0; i< stats.length;i++) {
 
-// }
+        if(stats[i][1] != ""){
+
+            if(stats[i][0]=="spd")
+            {
+                gs += parseFloat(stats[i][1]*2);
+            }
+            else if(stats[i][0]=="cdmg")
+            {
+                gs += parseFloat(stats[i][1]*1.15);
+            }
+            else if(stats[i][0]=="cc")
+            {
+                gs += parseFloat(stats[i][1]*1.6);
+            }
+            else
+            {
+                gs += parseFloat(stats[i][1]);
+            }
+        }
+    }
+    document.getElementById("gsResult").innerHTML = gs.toFixed(2);
+
+    //load_card(gs);
+}
